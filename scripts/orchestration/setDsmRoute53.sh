40d9e0061e5f247a2ab423de1fdce6140ab27046
424D57 <EMAIL ADDRESS>
2017-11-10 12:23:45 -0800

    Update aws cli query to match new cfn stack output name

--- scripts/orchestration/setDsmRoute53.sh
+++ scripts/orchestration/setDsmRoute53.sh
@@ -6,7 +6,7 @@ baseDomainHostedZoneId=${3}
 ##todo: get keys for trenddemos or lookup parameterized hosted zone
 
 dsmurl=$(aws cloudformation describe-stacks --stack-name ${stackname} --query 'Stacks[0].Outputs[?OutputKey==`DeepSecurityConsole`].OutputValue' --output text)
-dsmPublicElbHostedZoneId=$(aws cloudformation describe-stacks --stack-name ${stackname} --query 'Stacks[0].Outputs[?OutputKey==`PublicELBCanonicalHostedZoneNameID`].OutputValue' --output text)
+dsmPublicElbHostedZoneId=$(aws cloudformation describe-stacks --stack-name ${stackname} --query 'Stacks[0].Outputs[?OutputKey==`ELBCanonicalHostedZoneNameID`].OutputValue' --output text)
 elbfqdn=$(echo $dsmurl | cut -d'/' -f3 | cut -d':' -f1)
 aws route53 change-resource-record-sets --cli-input-json '{
   "HostedZoneId": "'${baseDomainHostedZoneId}'",
