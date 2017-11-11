1669c2639da325022368c5fe2115dddf0886269d
424D57 <EMAIL ADDRESS>
2017-11-11 07:06:23 -0600

    Update aws cli query to match new cfn stack output elbDns name

--- scripts/orchestration/setDsmCert.sh
+++ scripts/orchestration/setDsmCert.sh
@@ -3,7 +3,7 @@ stackname=${1}
 certid=${2}
 
 
-elbname=$(aws cloudformation describe-stacks --stack-name ${stackname} --query 'Stacks[].[Outputs[?OutputKey==`PublicELBDNSName`].OutputValue[]]' --output text)
+elbname=$(aws cloudformation describe-stacks --stack-name ${stackname} --query 'Stacks[].[Outputs[?OutputKey==`ELBDNSName`].OutputValue[]]' --output text)
 shortname=${elbname%-*-*-*}
 
 aws elb set-load-balancer-listener-ssl-certificate --load-balancer-name ${shortname} --load-balancer-port 443 --ssl-certificate-id ${certid}
